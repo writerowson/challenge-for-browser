e14eef716a11455ce155069bb3475961f82dac6c
coderowson <runurowson@gmail.com>
2022-03-08 03:46:10 +0600

    problemOne

--- problem01.js
+++ problem01.js
@@ -1,20 +1,30 @@
-
 // ১. setTimeout এর ভিতরে একটা কিছু কনসোল লগ করবে যেটা সাড়ে তিন সেকেন্ড পরে আউটপুট দেখাবে। 
-
-
 // my key
 const key = '26014200-c8cd57a2c8600f178e14e8426'
 // console.log(key)
 
 const showBtn = (id) => {
     const inputFeild = document.getElementById('input-result')
-    const inputText = inputFeild.value
+    const inputBTN = inputFeild.value
     // console.log(inputText)
     const url = `https://pixabay.com/api/?key=${key}=${id}&image_type=photo&pretty=true`
     fetch(url)
         .then(res => res.json())
         .then(data => displayImage(data))
 }
+const displayImage = ({ hits }) => {
+    const displayImg = document.getElementById('showImg')
+    hits.forEach((Image) => {
+        let div = document.createElement('div')
+        div.classList = "container col-lg-4"
+        div.innerHTML = ` <img class="img-fluid mt-5" onclick=selectItem(event,"${Image.webformatURL}") src="${Image.webformatURL}" alt="${Image.tags}">`
+        console.log(div.classList)
+        console.log(div.innerHTML)
+        displayImg.appendChild(div)
+    });
+}
+addEventListener("setTimeout", showBtn, 50000)
+
 
 // const displayImage = (Images) => {
 //     console.log(Images)
@@ -30,17 +40,3 @@ const showBtn = (id) => {
 //         console.log(Image)
 //     });
 // }    -----or-----
-const displayImage = ({ hits }) => {
-    const displayImg = document.getElementById('showImg')
-    hits.forEach((Image) => {
-        let div = document.createElement('div')
-
-        div.classList = "container col-lg-4"
-        div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${Image.webformatURL}") src="${Image.webformatURL}" alt="${Image.tags}">`
-        console.log(div.classList)
-        console.log(div.innerHTML)
-        displayImg.appendChild(div)
-    });
-}
-
-setTimeout(displayImage, 10000)
